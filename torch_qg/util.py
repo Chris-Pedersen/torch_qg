0946ca914a13a17bef380ab0d995bcf4040e9987
Chris-Pedersen <c.pedersen@nyu.edu>
2023-11-28 23:12:37 -0500

    Update animation object - now just takes a dataset and plots animation of the full dataset, reading relevant parameters from the dataset (will also work with pyqg datasets in current format)

--- torch_qg/util.py
+++ torch_qg/util.py
@@ -4,39 +4,44 @@ from IPython.display import HTML
 import cmocean
 import numpy as np
 
+YEAR = 24*60*60*360.
+
 class SimAnimation():
-    def __init__(self,q_tensor,fps=10,nSteps=1000,save_string=None):
-        self.q=q_tensor
-        self.fps = fps
-        self.nSteps = nSteps
-        self.save_string = save_string
+    def __init__(self,ds,fps=10,save_string=None):
+        self.ds=ds
+        self.nSteps=len(self.ds.q)
+        self.fps=fps
+        self.save_string=save_string
     
     def animate_func(self,i):
         if i % self.fps == 0:
             print( '.', end ='' )
     
         ## Set image and colorbar for each panel
-        image=self.q[i,0].numpy()
+        image=self.ds.q[i,0]
         self.ax1.set_array(image)
         self.ax1.set_clim(-np.max(np.abs(image)), np.max(np.abs(image)))
         
-        image=self.q[i,1].numpy()
+        image=self.ds.q[i,1]
         self.ax2.set_array(image)
         self.ax2.set_clim(-np.max(np.abs(image)), np.max(np.abs(image)))
+        
+        self.time_text.set_text("time=%.2f (years)" % (ds.time[i].to_numpy()/YEAR))
         
         return 
     
     def animate(self):
         fig, (axs1,axs2) = plt.subplots(1, 2,figsize=(14,6))
-        self.ax1=axs1.imshow(self.q[0,0], cmap=cmocean.cm.balance)
+        self.ax1=axs1.imshow(self.ds.q[0,0], cmap=cmocean.cm.balance)
         fig.colorbar(self.ax1, ax=axs1)
         axs1.set_xticks([]); axs1.set_yticks([])
         axs1.set_title("Upper")
 
-        self.ax2=axs2.imshow(self.q[0,1], cmap=cmocean.cm.balance)
+        self.ax2=axs2.imshow(self.ds.q[0,1], cmap=cmocean.cm.balance)
         fig.colorbar(self.ax1, ax=axs2)
         axs2.set_xticks([]); axs2.set_yticks([])
         axs2.set_title("Lower")
+        self.time_text=axs2.text(-20,-20,"")
         
         anim = animation.FuncAnimation(
                                        fig, 
@@ -45,8 +50,8 @@ class SimAnimation():
                                        interval = 1000 / self.fps, # in ms
                                        )
         # saving to m4 using ffmpeg writer 
-        writervideo = animation.FFMpegWriter(fps=60)
-        if self.save_string:
+        if self.save_string is not None:
+            writervideo = animation.FFMpegWriter(fps=60)
             anim.save('%s.mp4' % self.save_string, writer=writervideo) 
         plt.close()
         
